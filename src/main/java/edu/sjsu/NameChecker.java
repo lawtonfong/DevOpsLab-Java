cf5802f2bba35b036fa6e584fd6d04214d71c8f9
Lawton Fong <57239569+lawtonfong@users.noreply.github.com>
2024-04-24 18:59:07 -0700

    Update NameChecker.java
    updated NameChecker

--- src/main/java/edu/sjsu/NameChecker.java
+++ src/main/java/edu/sjsu/NameChecker.java
@@ -12,7 +12,27 @@ public class NameChecker {
      */
     public static boolean check(String input) {
         // TODO: implement
+         if (input.length() < 2 || input.length() > 40) {
+            return false;
+        }
 
-        return false;
+        //check if string contains only alphabetic characters, non-consecutive hyphens, or a single quote
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (!Character.isLetter(c) && c != '-' && c != '\'') {
+                return false;
+            }
+            if ((c == '-' || c == '\'') && (i == 0 || i == input.length() - 1)) {
+                // Hyphen or single quote cannot be the first or last character
+                return false;
+            }
+            if (c == '-' && input.charAt(i - 1) == '-') {
+                // Hyphens cannot be consecutive
+                return false;
+            }
+        }
+
+        return true;
+        
     }
 }
